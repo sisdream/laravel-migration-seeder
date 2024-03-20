8924c0d9ba3893c19282929bae17c8596a30265e
sisdream <sissipascale2@gmail.com>
2024-03-20 16:34:07 +0100

    feat: crata tabella

--- laravel/database/migrations/2024_03_20_150437_trains.php
+++ laravel/database/migrations/2024_03_20_150437_trains.php
@@ -15,7 +15,15 @@ return new class extends Migration
     {
         Schema::create("trains", function (Blueprint $table){
             $table->id();
-            
+            $table->string("Azienda");
+            $table->string("Stazione di partenza");
+            $table->string("Stazione di arrivo");
+            $table->timestamps("Orario di partenza");
+            $table->timestamps("Orario di arrivo");
+            $table->timestamps("Codice Treno");
+            $table->timestamp("Numero Carrozze");
+            $table->timestamp("In orario");
+            $table->string("Cancellato");
         });
     }
 
