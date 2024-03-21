273aff6d74981dfd6263a5e091dc659d461ded4c
sisdream <sissipascale2@gmail.com>
2024-03-21 17:38:37 +0100

    faet: ceata condizione

--- laravel/database/seeders/TrainSeeder.php
+++ laravel/database/seeders/TrainSeeder.php
@@ -16,32 +16,28 @@ class TrainSeeder extends Seeder
     public function run()
     {
         $file = fopen(__DIR__ . "../csv/train.csv","r");
+
+        $first_line = true;
         while (!feof($file)) {
-            $train_data = fgetcsv($file);
-            $train = new Trains;
+            if ($first_line) {
+                $train_data = fgetcsv($file);
+                $train = new Trains;
+                $train->id = $train_data[0];
+                $train->azienda = $train_data[1];
+                $train->stazione_partenza = $train_data[2];
+                $train->stazione_arrivo = $train_data[3];
+                $train->orario_partenza = $train_data[4];
+                $train->orario_arrivo = $train_data[5];
+                $train->codice_treno = $train_data[6];
+                $train->numero_carrozze = $train_data[7];
+                $train->in_orario = $train_data[8];
+                $train->cancellato = $train_data[9];
+                $train->timestamps = false;
+                $train->save();
+            }
 
-            $train->id = $train_data[0];
-            $train->azienda = $train_data[1];
-            $train->stazione_partenza = $train_data[2];
-            $train->stazione_arrivo = $train_data[3];
-            $train->orario_partenza = $train_data[4];
-            $train->orario_arrivo = $train_data[5];
-            $train->codice_treno = $train_data[6];
-            $train->numero_carrozze = $train_data[7];
-            $train->in_orario = $train_data[8];
-            $train->cancellato = $train_data[9];
-
-            $train->timestamps = false;
-            $train->save();
+            $first_line = false;
         }
         
-        // foreach($trains_data as $train_data){
-        //     $train = new Trains;
-
-        //     $train->fill($train_data);
-    
-        //     $train->timestamps = false;
-        //     $train->save();
-        // }
     }
 }
